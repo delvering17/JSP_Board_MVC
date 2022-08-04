0200777e79e2819ec88d4612cdadad1f6d0d5da8
delvering17 <delvering17@gmail.com>
2022-08-04 23:56:19 +0900

    4-2. BoardController에서 service class없는 구조 설정

--- JSP_Board_MVC/src/main/java/board_p/BoardController.java
+++ JSP_Board_MVC/src/main/java/board_p/BoardController.java
@@ -4,16 +4,35 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet("/board/*")
 public class BoardController extends HttpServlet {
+
+    HashMap<String,String> nonService = new HashMap<>();
+
+    public BoardController() {
+
+        nonService.put("BoardInsertForm","board/insertForm.jsp");
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String service = request.getRequestURI().substring((request.getContextPath()+"/board/").length());
         try {
 
-            BoardService bs = (BoardService) Class.forName("board_p." + service).newInstance();
-            bs.execute(request, response);
+
+
+
+
+
+            if (nonService.containsKey(service)) {
+                request.setAttribute("mainUrl", nonService.get(service));
+            } else {
+                BoardService bs = (BoardService) Class.forName("board_p." + service).newInstance();
+                bs.execute(request, response);
+            }
 
             RequestDispatcher dispatcher = request.getRequestDispatcher("/view/template.jsp");
             dispatcher.forward(request, response);
