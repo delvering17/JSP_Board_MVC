0fae71199d979c708898a70a16ae5159a99b15a9
delvering17 <delvering17@gmail.com>
2022-08-08 12:59:00 +0900

    6-6. BoardModifyReg service 작성

---
+++ JSP_Board_MVC/src/main/java/board_p/BoardModifyReg.java
@@ -0,0 +1,70 @@
+package board_p;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import model_p.BoardDAO;
+import model_p.BoardDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BoardModifyReg implements BoardService {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+        String path = request.getRealPath("/fff");
+        path = "/Users/song-chanwook/workplace/git_repo/JSP_practice/mvcProj/src/main/webapp/fff";
+
+        int size = 1024*1024 *10;
+        try {
+            MultipartRequest mr = new MultipartRequest(
+                    request,
+                    path,
+                    size,
+                    "UTF-8",
+                    new DefaultFileRenamePolicy());
+
+            BoardDTO dto = new BoardDTO();
+            dto.setId(Integer.parseInt(mr.getParameter("id")));
+            dto.setTitle(mr.getParameter("title"));
+            dto.setPname(mr.getParameter("pname"));
+            dto.setPw(mr.getParameter("pw"));
+            dto.setContent(mr.getParameter("content"));
+
+            if(mr.getParameter("upfile") != null) {
+                dto.setUpfile(mr.getParameter("upfile"));
+            } else {
+                dto.setUpfile(mr.getFilesystemName("upfile"));
+            }
+
+            int cnt = new BoardDAO().modify(dto);
+
+            String msg = "수정 실패";
+            String mainUrl = "board/modifyForm.jsp";
+
+
+            if(cnt>0) {
+
+                msg = "수정 성공";
+                mainUrl = "board/alert.jsp";
+                request.setAttribute("goUrl","BoardDetail?id="+dto.getId());
+
+            }
+
+            System.out.println(dto);
+
+            request.setAttribute("mainUrl", mainUrl);
+            request.setAttribute("msg", msg);
+            request.setAttribute("dto", dto);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
