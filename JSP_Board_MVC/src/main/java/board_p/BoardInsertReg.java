3856150240a12a8e1d9a6b66548a573781420774
delvering17 <delvering17@gmail.com>
2022-08-09 15:01:14 +0900

    8-13.[BoardInsertReg] 수정(page attribute 넘겨줌)

--- JSP_Board_MVC/src/main/java/board_p/BoardInsertReg.java
+++ JSP_Board_MVC/src/main/java/board_p/BoardInsertReg.java
@@ -1,31 +1,32 @@
 package board_p;
 
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
-import model_p.BoardDAO;
-import model_p.BoardDTO;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-public class BoardInsertReg implements BoardService{
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import model_p.BoardDAO;
+import model_p.BoardDTO;
+
+public class BoardInsertReg implements BoardService {
+
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
 
         String path = request.getRealPath("/fff");
-        path = "/Users/song-chanwook/workplace/git_repo/JSP_Board_MVC/JSP_Board_MVC/src/main/webapp/fff";
+        path = "/Users/song-chanwook/workplace/git_repo/JSP_practice/mvcProj/src/main/webapp/fff";
 
-        int size = 1024 * 1024 * 10;
-
+        int size = 1024*1024 *10;
         try {
             MultipartRequest mr = new MultipartRequest(
                     request,
                     path,
                     size,
                     "UTF-8",
-                    new DefaultFileRenamePolicy()
-            );
+                    new DefaultFileRenamePolicy());
 
             BoardDTO dto = new BoardDTO();
             dto.setTitle(mr.getParameter("title"));
@@ -34,17 +35,21 @@ public class BoardInsertReg implements BoardService{
             dto.setUpfile(mr.getFilesystemName("upfile"));
             dto.setContent(mr.getParameter("content"));
 
+
             new BoardDAO().insert(dto);
 
+
+//            System.out.println(dto);
 
             request.setAttribute("mainUrl", "board/alert.jsp");
             request.setAttribute("msg", "입력되었습니다.");
             request.setAttribute("goUrl", "BoardDetail?id="+dto.getId());
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-
     }
+
 }
