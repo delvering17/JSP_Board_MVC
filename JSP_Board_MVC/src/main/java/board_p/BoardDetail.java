25b6aba29476dd5b3faeb751fc4c17d639f6ad33
delvering17 <delvering17@gmail.com>
2022-08-09 14:51:41 +0900

    8-6.[BoardDetail] 수정(page attribute 넘겨줌)

--- JSP_Board_MVC/src/main/java/board_p/BoardDetail.java
+++ JSP_Board_MVC/src/main/java/board_p/BoardDetail.java
@@ -5,18 +5,29 @@ import model_p.BoardDTO;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
-public class BoardDetail implements BoardService{
+public class BoardDetail implements BoardService {
+
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
 
         int id = Integer.parseInt(request.getParameter("id"));
+        System.out.println(id);
 
         BoardDAO dao = new BoardDAO();
         dao.addCnt(id);
         BoardDTO dto = dao.detail(id);
 
-        request.setAttribute("dto",dto);
-        request.setAttribute("mainUrl","board/detail.jsp");
+//		System.out.println(dto);
+
+        int nowPage = Integer.parseInt(request.getParameter("nowPage"));
+
+        request.setAttribute("nowPage",nowPage);
+        request.setAttribute("mainUrl", "board/detail.jsp");
+        request.setAttribute("dto", dto);
+
+
     }
+
 }
