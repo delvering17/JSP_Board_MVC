54bff40fde000023407010753b70a27dcba74e35
delvering17 <delvering17@gmail.com>
2022-08-09 13:14:55 +0900

    8-3.[BoardList] 수정(page 설정)

--- JSP_Board_MVC/src/main/java/board_p/BoardList.java
+++ JSP_Board_MVC/src/main/java/board_p/BoardList.java
@@ -14,10 +14,29 @@ public class BoardList implements BoardService{
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
 
-        System.out.println("BoardList 진입");
+        int nowPage = (int) request.getAttribute("nowPage");
+        int limit = 5;
+        int pageLimit = 4;
+        int total = new BoardDAO().total();
+        int totalPage = total / limit;
+        int first = (nowPage - 1) * limit;
+        int firstPage = (nowPage - 1) / pageLimit * pageLimit + 1;
+        int endPage = firstPage + pageLimit - 1;
 
-        ArrayList<BoardDTO> mainData = new BoardDAO().list();
+        if((total % limit) != 0) {
+            totalPage ++;
+        }
 
+        if(endPage > totalPage) {
+            endPage = totalPage;
+        }
+
+        ArrayList<BoardDTO> mainData = new BoardDAO().list(first, limit);
+
+        request.setAttribute("first", first);
+        request.setAttribute("firstPage", firstPage);
+        request.setAttribute("endPage", endPage);
+        request.setAttribute("totalPage", totalPage);
         request.setAttribute("mainData",mainData);
         request.setAttribute("mainUrl", "board/list.jsp");
 
